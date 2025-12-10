0057230ae0e91bfbf34afc5d122bed572d42a214
Jakke Korpelainen <3626319+jakke-korpelainen@users.noreply.github.com>
2025-12-10 21:31:00 +0200

    refactor: drop obsolete cache file logic

--- src/file.rs
+++ src/file.rs
@@ -2,7 +2,6 @@ use std::path::PathBuf;
 use tokio::fs::{File, OpenOptions};
 
 const CLI_DIR: &str = ".config/jwekke-ai-cli";
-const CACHE_FILE_NAME: &str = "cache.jwek";
 const CONFIG_FILE_NAME: &str = "config.jwek";
 
 pub fn get_cli_config_dir() -> PathBuf {
@@ -13,52 +12,6 @@ pub fn get_cli_config_dir() -> PathBuf {
         Ok(_) => dir_path,
         Err(e) => {
             panic!("Failed to create config directory: {}", e);
-        }
-    }
-}
-
-pub fn get_cache_file_path() -> PathBuf {
-    let dir_path = get_cli_config_dir();
-    dir_path.join(CACHE_FILE_NAME)
-}
-
-pub fn get_config_file_path() -> PathBuf {
-    let dir_path = get_cli_config_dir();
-    dir_path.join(CONFIG_FILE_NAME)
-}
-
-pub async fn open_config_file() -> (PathBuf, File) {
-    let config_file_path = get_config_file_path();
-
-    match open_file(config_file_path).await {
-        Ok((file_path, file)) => (file_path, file),
-        Err(e) => {
-            panic!("Failed to open config file: {}", e);
-        }
-    }
-}
-
-pub async fn open_cache_file() -> (PathBuf, File) {
-    let cache_file_path = get_cache_file_path();
-
-    match open_file(cache_file_path).await {
-        Ok((file_path, file)) => (file_path, file),
-        Err(e) => {
-            panic!("Failed to open cache file: {}", e);
-        }
-    }
-}
-
-pub async fn open_file(file_path: PathBuf) -> Result<(PathBuf, File), std::io::Error> {
-    match OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(&file_path)
-        .await
-    {
-        Ok(file) => Ok((file_path, file)),
-        Err(e) => {
-            panic!("Failed to open file: {:?}, error: {}", file_path, e);
         }
     }
 }
@@ -81,11 +34,33 @@ pub async fn create_file(
     }
 }
 
-pub async fn create_cache_file() -> (PathBuf, File) {
-    let cache_file_path = get_cache_file_path();
-    match create_file(cache_file_path, true).await {
-        Ok(file) => file,
-        Err(e) => panic!("Failed to create cache file: {}", e),
+pub async fn open_file(file_path: PathBuf) -> Result<(PathBuf, File), std::io::Error> {
+    match OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&file_path)
+        .await
+    {
+        Ok(file) => Ok((file_path, file)),
+        Err(e) => {
+            panic!("Failed to open file: {:?}, error: {}", file_path, e);
+        }
+    }
+}
+
+pub fn get_config_file_path() -> PathBuf {
+    let dir_path = get_cli_config_dir();
+    dir_path.join(CONFIG_FILE_NAME)
+}
+
+pub async fn open_config_file() -> (PathBuf, File) {
+    let config_file_path = get_config_file_path();
+
+    match open_file(config_file_path).await {
+        Ok((file_path, file)) => (file_path, file),
+        Err(e) => {
+            panic!("Failed to open config file: {}", e);
+        }
     }
 }
 
